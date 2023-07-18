6e5fe5eecdc577dc437ef4761241b3bd47f2f58f
unknown <omidshayan.afg@gmail.com>
2023-07-18 06:51:34 +0430

    user panel

---
+++ users/panel.php
@@ -0,0 +1,39 @@
+<?php
+session_start();
+include_once 'sidebar.php'; 
+?>
+
+                <main>
+
+                    <div class="overview">
+                        <div class="all-review">صفحه اصلی</div>
+
+
+
+<!-- 
+
+                                                <div class="overview-item">
+                            <div class="overview-icon">
+                                <i class="fas fa-align-center"></i>
+                            </div>
+                            <div class="overview-text">
+                                <h4>تعداد تمام اجناس</h4>
+                                    <h5>5 adad</h5>
+                            </div>
+                        </div>
+                        
+                    </div> -->
+
+                    
+
+
+
+                                </main>
+
+        </div>
+
+   <!-- js library -->
+    <script src="../js/fontA.js"></script>
+    <script src="../js/custom.js"></script>
+</body>
+</html>
