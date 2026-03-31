05d1adb69e45a51b646f800873a81890ec1e5d93
omidshayan <omidshayan.dev@gmail.com>
2026-04-01 01:46:44 +0430

    v1 form

--- resources/views/app/users/add-user.php
+++ resources/views/app/users/add-user.php
@@ -12,55 +12,127 @@
 
                     <div class="inputs d-flex">
                         <div class="one">
-                            <div class="label-form mb5 fs14">نام و تخلص <?= _star ?> </div>
+                            <div class="label-form mb5 fs14">نام مشتری <?= _star ?> </div>
                             <input type="text" class="checkInput" name="user_name" placeholder="نام و تخلص را وارد نمایید" maxlength="40" />
                         </div>
                         <div class="one">
-                            <div class="label-form mb5 fs14">نام شرکت</div>
-                            <input type="text" class="checkInput" name="company" placeholder="نام شرکت را وارد نمایید" maxlength="40" />
+                            <div class="label-form mb5 fs14">شماره <?= _star ?></div>
+                            <input type="text" class="checkInput" name="company" placeholder="شماره را وارد نمایید" maxlength="40" />
                         </div>
                     </div>
 
-                    <div class="inputs d-flex">
-                        <div class="one">
-                            <div class="label-form mb5 fs14">شماره <span class="fs12 color-orange">(تلگرام)</span> <?= _star ?> </div>
-                            <input type="number" class="checkInput" name="phone" placeholder="شماره را وارد نمایید" />
-                        </div>
-                        <div class="one">
-                            <div class="label-form mb5 fs14">باقیداری گذشته </div>
-                            <input type="text" name="remnants_past" placeholder="باقیداری قبلی مشتری را وارد نمایید" maxlength="40" />
+                    <div class="accordion-title color-orange mb5">ثبت اندزه‌ها</div>
+                    <div class="accordion-content">
+                        <div class="child-accordioin">
+
+                            <div class="inputs d-flex">
+                                <div class="one">
+                                    <div class="label-form mb5 fs14">قد</div>
+                                    <input type="text" placeholder=" اندازه قد را وارد نمایید ..." name="customer_height">
+                                </div>
+                                <div class="one">
+                                    <div class="label-form mb5 fs14">شانه</div>
+                                    <input type="text" placeholder="اندازه شانه را وارد نمایید ..." name="customer_sholder">
+                                </div>
+                            </div>
+
+                            <div class="inputs d-flex">
+                                <div class="one">
+                                    <div class="label-form mb5 fs14">آستین</div>
+                                    <input type="text" placeholder="اندازه آستین را وارد نمایید ..." name="customer_sleeve">
+                                </div>
+                                <div class="one">
+                                    <div class="label-form mb5 fs14">هخن</div>
+                                    <input type="text" placeholder="اندازه هخن را وارد نمایید ..." name="customer_ice">
+                                </div>
+                            </div>
+
+                            <div class="inputs d-flex">
+                                <div class="one">
+                                    <div class="label-form mb5 fs14">بغل</div>
+                                    <input type="text" placeholder="اندازه بغل را وارد نمایید ..." name="customer_hug">
+                                </div>
+                                <div class="one">
+                                    <div class="label-form mb5 fs14">دامن</div>
+                                    <input type="text" placeholder="اندازه دامن را وارد نمایید ..." name="customer_skirt">
+                                </div>
+                            </div>
+
+                            <div class="inputs d-flex">
+                                <div class="one">
+                                    <div class="label-form mb5 fs14">چتی</div>
+                                    <input type="text" placeholder="اندازه چتی را وارد نمایید ..." name="customer_chatty">
+                                </div>
+                                <div class="one">
+                                    <div class="label-form mb5 fs14">شلوار</div>
+                                    <input type="text" placeholder="اندازه شلوار را وارد نمایید ..." name="customer_pants">
+                                </div>
+                            </div>
+
+                            <div class="inputs d-flex">
+                                <div class="one">
+                                    <div class="label-form mb5 fs14">پارچه</div>
+                                    <input type="text" placeholder="اندازه پارچه را وارد نمایید ..." name="customer_cloth">
+                                </div>
+                                <div class="one">
+                                    <div class="label-form mb5 fs14">بر‌ شلوار</div>
+                                    <input type="text" placeholder="اندازه بر شلوار را وارد نمایید ..." name="customer_bar_pants">
+                                </div>
+                            </div>
+
+                            <div class="inputs d-flex">
+                                <div class="one">
+                                    <div class="label-form mb5 fs14">مدل</div>
+                                    <input type="text" placeholder="مدل را وارد نمایید ..." name="customer_model">
+                                </div>
+                                <div class="one">
+                                    <div class="label-form mb5 fs14">توضیحات دوخت</div>
+                                    <input type="type" placeholder="توضیحات را وارد نمایید ..." name="customer_size_desc">
+                                </div>
+                            </div>
+
                         </div>
                     </div>
 
-                    <div class="inputs d-flex">
-                        <div class="one">
-                            <div class="label-form mb5 fs14">آدرس</div>
-                            <textarea name="address" placeholder="آدرس را وارد نمایید"></textarea>
-                        </div>
-                        <div class="one">
-                            <div class="label-form mb5 fs14">توضیحات</div>
-                            <textarea name="description" placeholder="توضیحات را وارد نمایید"></textarea>
-                        </div>
-                    </div>
+                    <div class="accordion-title color-orange mb5">ثبت اندازه‌های واسکت</div>
+                    <div class="accordion-content">
+                        <div class="child-accordioin">
 
-                    <div class="inputs d-flex d-none">
-                        <div class="one ">
-                            <label class="d-flex justify-center">
-                                <input type="checkbox" class="checkbox-select" name="is_customer" checked>
-                                <div class="label-form mb5 fs22" for="name">مشتری</div>
-                            </label>
-                        </div>
-                        <div class="one ">
-                            <label class="d-flex justify-center">
-                                <input type="checkbox" class="checkbox-select" name="is_seller" checked>
-                                <div class="label-form mb5 fs22" for="name">فروشنده</div>
-                            </label>
+                            <div class="inputs d-flex">
+                                <div class="one">
+                                    <div class="label-form mb5 fs14">قد</div>
+                                    <input type="text" placeholder="اندزه قد را وارد نمایید ..." name="vaskat_height">
+                                </div>
+                                <div class="one">
+                                    <div class="label-form mb5 fs14">شانه</div>
+                                    <input type="text" placeholder="اندازه شانه را وارد نمایید ..." name="vaskat_sholder">
+                                </div>
+                            </div>
+                            <div class="inputs d-flex">
+                                <div class="one">
+                                    <div class="label-form mb5 fs14">چتی</div>
+                                    <input type="text" placeholder="اندازه چتی را وارد نمایید ..." name="vaskat_chatty">
+                                </div>
+                                <div class="one">
+                                    <div class="label-form mb5 fs14">توضیحات واسکت</div>
+                                    <input type="text" placeholder="توضیحات را وارد نمایید ..." name="vaskat_desc">
+                                </div>
+                            </div>
+
                         </div>
                     </div>
 
                     <div class="accordion-title color-orange">جزئیات بیشتر</div>
                     <div class="accordion-content">
                         <div class="child-accordioin">
+
+                            <div class="inputs d-flex">
+                                <div class="one">
+                                    <div class="label-form mb5 fs14">شماره مشتری <?= _star ?> </div>
+                                    <input type="text" placeholder="شماره موبایل مشتری را وارد نمایید  ..." name="customer_phone" required>
+                                </div>
+                            </div>
+
                             <div class="inputs d-flex">
                                 <div class="one">
                                     <div class="label-form mb5 fs14">نام معرف </div>
@@ -69,6 +141,17 @@
                                 <div class="one">
                                     <div class="label-form mb5 fs14">شماره معرف <?= _star ?> </div>
                                     <input type="number" name="reagent_phone" placeholder="شماره معرف را وارد نمایید" />
+                                </div>
+                            </div>
+
+                            <div class="inputs d-flex">
+                                <div class="one">
+                                    <div class="label-form mb5 fs14">آدرس</div>
+                                    <textarea name="address" placeholder="آدرس را وارد نمایید"></textarea>
+                                </div>
+                                <div class="one">
+                                    <div class="label-form mb5 fs14">توضیحات</div>
+                                    <textarea name="description" placeholder="توضیحات را وارد نمایید"></textarea>
                                 </div>
                             </div>
 
