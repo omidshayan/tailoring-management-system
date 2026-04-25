2ea10b194edadd4b5136c8f8e57dcf54cc35ae04
omidshayan <omidshayan.dev@gmail.com>
2026-04-25 18:33:14 +0430

    end show models

--- resources/views/app/basic-sections/models/models.php
+++ resources/views/app/basic-sections/models/models.php
@@ -1,16 +1,12 @@
-<!-- start sidebar -->
 <?php
-$title = 'مدیریت بخش کمپانی‌ها';
+$title = 'مدیریت مدل‌های لباس';
 include_once('resources/views/layouts/header.php');
 include_once('public/alerts/check-inputs.php');
-// include_once('public/alerts/error.php');
 include_once('public/alerts/toastr.php');
 ?>
-<!-- end sidebar -->
 
-<!-- Start content -->
 <div class="content">
-    <div class="content-title"> مدیریت بخش کمپانی‌ها
+    <div class="content-title"> مدیریت مدل‌های لباس
         <span class="help fs14 text-underline cursor-p color-orange" id="openModalBtn">(راهنما)</span>
     </div>
     <?php
@@ -21,17 +17,12 @@ include_once('public/alerts/toastr.php');
     <!-- start page content -->
     <div class="box-container">
         <div class="insert">
-            <form id="myForm" action="company-store" method="POST">
+            <form id="myForm" action="model-store" method="POST">
                 <div class="inputs d-flex">
                     <div class="one">
-                        <div class="label-form mb5 fs14">نام کمپانی‌ <?= _star ?> </div>
-                        <input type="text" name="company_name" class="checkInput" value="<?= old('name') ?>" placeholder="نام نام کمپانی را وارد نمایید" autocomplete="off" />
+                        <div class="label-form mb5 fs14">نام مدل <?= _star ?> </div>
+                        <input type="text" name="company_name" class="checkInput" value="<?= old('name') ?>" placeholder="نام  مدل را وارد نمایید" autocomplete="off" />
                     </div>
-                    <div class="one">
-                        <div class="label-form mb5 fs14">توضیحات</div>
-                        <textarea name="description" placeholder="توضیحات را وارد نمایید"></textarea>
-                    </div>
-                    <?= $this->branchSelectField(); ?>
                 </div>
 
                 <input type="hidden" name="csrf_token" value="<?= $_SESSION['csrf_token'] ?>">
@@ -40,15 +31,14 @@ include_once('public/alerts/toastr.php');
         </div>
     </div>
 
-    <!-- show packages -->
+    <!-- show models -->
     <div class="box-container">
-        <div class="mb10 fs14"> واحد‌های ثبت شده</div>
+        <div class="mb10 fs14"> مدل‌های ثبت شده</div>
         <table class="fl-table">
             <thead>
                 <tr>
                     <th>#</th>
-                    <th>نام واحد</th>
-                    <th>وضعیت</th>
+                    <th>نام مدل</th>
                     <th>ویرایش</th>
                     <th>جزئیات</th>
                 </tr>
@@ -56,21 +46,16 @@ include_once('public/alerts/toastr.php');
             <tbody>
                 <?php
                 $perPage = 10;
-                $data = paginate($companies, $perPage);
+                $data = paginate($models, $perPage);
                 $currentPage = isset($_GET['page']) ? (int)$_GET['page'] : 1;
                 $number = ($currentPage - 1) * $perPage + 1;
                 foreach ($data as $item) {
                 ?>
                     <tr>
                         <td class="color-orange"><?= $number ?></td>
-                        <td><?= $item['company_name'] ?></td>
+                        <td><?= $item['af_model'] ?></td>
                         <td>
-                            <span class="status">
-                                <?= ($item['status'] == 1) ? '<span class="color-green">فعال</span>' : '<span class="color-red">غیرفعال</span>' ?>
-                            </span>
-                        </td>
-                        <td>
-                            <a href="<?= url('edit-company/' . $item['id']) ?>" class="color-orange">
+                            <a href="<?= url('edit-model/' . $item['id']) ?>" class="color-orange">
                                 <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-pencil-square" viewBox="0 0 16 16">
                                     <path d="M15.502 1.94a.5.5 0 0 1 0 .706L14.459 3.69l-2-2L13.502.646a.5.5 0 0 1 .707 0l1.293 1.293zm-1.75 2.456-2-2L4.939 9.21a.5.5 0 0 0-.121.196l-.805 2.414a.25.25 0 0 0 .316.316l2.414-.805a.5.5 0 0 0 .196-.12l6.813-6.814z" class="color-orange" />
                                     <path fill-rule="evenodd" d="M1 13.5A1.5 1.5 0 0 0 2.5 15h11a1.5 1.5 0 0 0 1.5-1.5v-6a.5.5 0 0 0-1 0v6a.5.5 0 0 1-.5.5h-11a.5.5 0 0 1-.5-.5v-11a.5.5 0 0 1 .5-.5H9a.5.5 0 0 0 0-1H2.5A1.5 1.5 0 0 0 1 2.5z" />
@@ -78,7 +63,7 @@ include_once('public/alerts/toastr.php');
                             </a>
                         </td>
                         <td>
-                            <a href="<?= url('company-details/' . $item['id']) ?>">
+                            <a href="<?= url('model-details/' . $item['id']) ?>">
                                 <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-eye" viewBox="0 0 16 16">
                                     <path d="M16 8s-3-5.5-8-5.5S0 8 0 8s3 5.5 8 5.5S16 8 16 8M1.173 8a13 13 0 0 1 1.66-2.043C4.12 4.668 5.88 3.5 8 3.5s3.879 1.168 5.168 2.457A13 13 0 0 1 14.828 8q-.086.13-.195.288c-.335.48-.83 1.12-1.465 1.755C11.879 11.332 10.119 12.5 8 12.5s-3.879-1.168-5.168-2.457A13 13 0 0 1 1.172 8z" />
                                     <path d="M8 5.5a2.5 2.5 0 1 0 0 5 2.5 2.5 0 0 0 0-5M4.5 8a3.5 3.5 0 1 1 7 0 3.5 3.5 0 0 1-7 0" class="color-orange" />
@@ -91,26 +76,23 @@ include_once('public/alerts/toastr.php');
                 }
                 ?>
             </tbody>
-            <tbody></tbody>
         </table>
         <div class="flex-justify-align mt20 paginate-section">
-            <div class="table-info fs12">تعداد کل: <?= count($companies) ?></div>
+            <div class="table-info fs12">تعداد کل: <?= count($models) ?></div>
             <?php
-            if (count($companies) == null) { ?>
+            if (count($models) == null) { ?>
                 <div class="center color-red fs12">
                     <i class="fa fa-comment"></i>
                     <?= _not_infos ?>
                 </div>
             <?php } else {
-                if (count($companies) > 10) {
-                    echo paginateView($companies, 10);
+                if (count($models) > 10) {
+                    echo paginateView($models, 10);
                 }
             }
             ?>
         </div>
     </div>
-    <!-- end page content -->
 </div>
-<!-- End content -->
 
 <?php include_once('resources/views/layouts/footer.php') ?>
