35b8876a249c25bb219a728c6336fc3ea1f67f96
omidshayan <omidshayan.dev@gmail.com>
2026-04-26 23:14:46 +0430

    add search box for search fabric

--- resources/views/app/fabrics/manage/buy-fabric.php
+++ resources/views/app/fabrics/manage/buy-fabric.php
@@ -1,31 +1,39 @@
     <?php
-    $title = 'ثبت پارچه جدید';
+    $title = 'ثبت خرید پارچه ';
     include_once('resources/views/layouts/header.php');
     include_once('public/alerts/check-inputs.php');
-    include_once('public/alerts/toastr.php'); ?>
+    include_once('public/alerts/toastr.php');
+    include_once('resources/views/scripts/live-search-items.php');
+    ?>
 
     <div class="content">
-        <div class="content-title">ثبت پارچه جدید</div>
+        <div class="content-title">ثبت خرید پارچه</div>
+
+        <!-- search box for fabric -->
+        <div class=" flex-justify-align mb10">
+            <div class="search-database-s flex-justify-align border"
+                data-url="<?= url('search-item') ?>"
+                data-input-id="search_user"
+                data-result-id="backResponseSeller"
+                data-field-name="customer_name"
+                data-target-id="item_id">
+                <a href="#" class="color search-icon-database-s">
+                    <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" class="ml-10 search-icon w17">
+                        <circle cx="11" cy="11" r="8"></circle>
+                        <line x1="21" y1="21" x2="16.65" y2="16.65"></line>
+                    </svg>
+                </a>
+                <input type="text" class="p5 fs15 input w100 border checkInput" id="search_user" placeholder="جستجوی پارچه" autofocus />
+                <ul class="search-back d-none top40" id="backResponseSeller">
+                    <li class="search-item color" role="option"></li>
+                </ul>
+            </div>
+        </div>
+
 
         <div class="box-container">
             <div class="insert">
                 <form action="<?= url('fabric-store') ?>" method="POST">
-                    <div class="inputs d-flex">
-                        <div class="one">
-                            <div class="label-form mb5 fs14">نام پارچه <?= _star ?> </div>
-                            <input type="text" class="checkInput" name="name" placeholder="نام پارچه را وارد نمایید" maxlength="40" />
-                        </div>
-                        <div class="one">
-                            <div class="label-form mb5 fs14">مدل پارچه</div>
-                            <select name="category">
-                                <option disabled selected>مدل پارچه را انتخاب نمایید</option>
-                                <option value="چینایی">چینایی</option>
-                                <option value="پاکستانی">پاکستانی</option>
-                                <option value="هندی">هندی</option>
-                                <option value="ایرانی">ایرانی</option>
-                            </select>
-                        </div>
-                    </div>
 
                     <div class="inputs d-flex">
                         <div class="one">
@@ -38,17 +46,7 @@
                         </div>
                     </div>
 
-                    <div class="inputs d-flex">
-                        <div class="one">
-                            <div class="label-form mb5 fs14">رنگ پارچه </div>
-                            <input type="text" name="color" placeholder="رنگ پارچه را وارد نمایید" />
-                        </div>
-                        <div class="one">
-                            <div class="label-form mb5 fs14">توضیحات</div>
-                            <textarea name="description" placeholder="توضیحات را وارد نمایید"></textarea>
-                        </div>
-                    </div>
-
+                    <input type="hidden" name="user_id" id="item_id">
                     <input type="hidden" name="csrf_token" value="<?= $_SESSION['csrf_token'] ?>" />
                     <input type="submit" id="submit" value="ثبت" class="btn" />
                 </form>
