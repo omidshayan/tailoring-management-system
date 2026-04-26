6f6a1bbbd2ebb76fe9ba5ebfaabd2968e7130c00
omidshayan <omidshayan.dev@gmail.com>
2026-04-26 19:37:09 +0430

    fabrics form

---
+++ resources/views/app/fabrics/add-fabric.php
@@ -0,0 +1,71 @@
+    <?php
+    $title = 'ثبت پارچه جدید';
+    include_once('resources/views/layouts/header.php');
+    include_once('public/alerts/check-inputs.php');
+    include_once('public/alerts/toastr.php'); ?>
+
+    <div class="content">
+        <div class="content-title">ثبت پارچه جدید</div>
+
+        <div class="box-container">
+            <div class="insert">
+                <form action="<?= url('employee-store') ?>" method="POST" enctype="multipart/form-data">
+                    <div class="inputs d-flex">
+                        <div class="one">
+                            <div class="label-form mb5 fs14">نام پارچه <?= _star ?> </div>
+                            <input type="text" class="checkInput" name="employee_name" placeholder="نام پارچه را وارد نمایید" maxlength="40" />
+                        </div>
+                        <div class="one">
+                            <div class="label-form mb5 fs14">مدل پارچه</div>
+                            <select name="">
+                                <option disabled selected>مدل پارچه را انتخاب نمایید</option>
+                                <option value="چینایی">چینایی</option>
+                                <option value="پاکستانی">پاکستانی</option>
+                                <option value="هندی">هندی</option>
+                                <option value="ایرانی">ایرانی</option>
+                            </select>
+                        </div>
+                    </div>
+                    <div class="inputs d-flex">
+                        <div class="one">
+                            <div class="label-form mb5 fs14">انتخاب فروشنده</div>
+                            <select name="">
+                                <option disabled selected>فروشنده را انتخاب نمایید</option>
+                                <option value="چینایی">چینایی</option>
+                                <option value="پاکستانی">پاکستانی</option>
+                                <option value="هندی">هندی</option>
+                                <option value="ایرانی">ایرانی</option>
+                            </select>
+                        </div>
+                        <div class="one">
+                            <div class="label-form mb5 fs14">رنگ پارچه </div>
+                            <input type="text" name="phone" placeholder="رنگ پارچه را وارد نمایید" />
+                        </div>
+                    </div>
+
+                    <div class="inputs d-flex">
+                        <div class="one">
+                            <div class="label-form mb5 fs14">قیمت خرید فی متر <?= _star ?></div>
+                            <input type="text" class="checkInput" name="phone" placeholder="رنگ پارچه را وارد نمایید" />
+                        </div>
+                        <div class="one">
+                            <div class="label-form mb5 fs14">قیمت فروش فی متر <?= _star ?></div>
+                            <input type="text" class="checkInput" name="phone" placeholder="رنگ پارچه را وارد نمایید" />
+                        </div>
+                    </div>
+
+                    <div class="inputs d-flex">
+                        <div class="one">
+                            <div class="label-form mb5 fs14">توضیحات</div>
+                            <textarea name="description" placeholder="توضیحات را وارد نمایید"></textarea>
+                        </div>
+                    </div>
+
+                    <input type="hidden" name="csrf_token" value="<?= $_SESSION['csrf_token'] ?>" />
+                    <input type="submit" id="submit" value="ثبت" class="btn" />
+                </form>
+            </div>
+        </div>
+    </div>
+
+    <?php include_once('resources/views/layouts/footer.php') ?>
