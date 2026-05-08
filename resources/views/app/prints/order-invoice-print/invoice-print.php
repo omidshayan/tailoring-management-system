9139fe0d92b65d3be46a1671f80560032c09af6c
omidshayan <omidshayan.dev@gmail.com>
2026-05-08 17:09:21 +0430

    count

--- resources/views/app/prints/order-invoice-print/invoice-print.php
+++ resources/views/app/prints/order-invoice-print/invoice-print.php
@@ -5,7 +5,7 @@
     <meta charset="UTF-8">
     <title>فاکتور خیاطی</title>
 
-      <link rel="stylesheet" href="<?= asset('resources/views/app/prints/print.css') ?>" />
+    <link rel="stylesheet" href="<?= asset('resources/views/app/prints/print.css') ?>" />
 
 </head>
 
@@ -14,9 +14,15 @@
     <div class="form-container border">
         <div class="order-header">
             <div class="order-header1 d-flex justify-between align-center">
-                <div class="header-name-fa fs30 bold">خیاطی و پارچه سرای آرمان</div>
-                <div class="header-name-en bold fs18">ARMAN TEXTILE STORE TAILOR</div>
+                <div class="fs30 bold">خیاطی و پارچه سرای آرمان</div>
+                <div class="bold fs18">ARMAN TEXTILE STORE TAILOR</div>
             </div>
+
+            <div class="order-header1 d-flex justify-between align-center">
+                <div class="fs18 bold">مدیریت: احمد شفیع احسان</div>
+                <div class="bold fs16">تماس‌ها: 0799999999 - 0799999999</div>
+            </div>
+
             <div class="order-header2"></div>
         </div>
     </div>
