6fcf663ed3f19a910cecaa2efcee2d159c7f525c
omidshayan <omidshayan.dev@gmail.com>
2026-05-08 16:29:24 +0430

    add heasder

--- resources/views/app/prints/order-invoice-print/invoice-print.php
+++ resources/views/app/prints/order-invoice-print/invoice-print.php
@@ -4,6 +4,8 @@
 <head>
     <meta charset="UTF-8">
     <title>فاکتور خیاطی</title>
+    <link rel="stylesheet" href="<?= asset('public/assets/style/style.css') ?>" />
+    <link rel="stylesheet" href="<?= asset('public/assets/style/main.css') ?>" />
 
     <style>
         body {
@@ -12,12 +14,11 @@
             background-color: #0706065d;
         }
 
-        .invoice {
-            width: 100%;
+        .order-invoice-print {
+            width: 40%;
             height: 7.5cm;
             border: 2px solid #000;
-            padding: 10px;
-            box-sizing: border-box;
+            background-color: green;
         }
     </style>
 
@@ -25,8 +26,14 @@
 
 <body>
 
-    <div class="invoice">
-
+    <div class="order-invoice-print">
+        <div class="order-header">
+            <div class="order-header1 d-flex">
+                <div class="header-name-en">ARMAN TEXTILE STORE TAILOR</div>
+                <div class="header-name-fa">خیاطی و پارچه سرای آرمان</div>
+            </div>
+            <div class="order-header2"></div>
+        </div>
     </div>
 
 </body>
