1eabb59b8a0b1ce374d2809f000152f63a5eaf54
omidshayan <omidshayan.dev@gmail.com>
2026-05-11 17:22:56 +0430

    start edit user

--- resources/views/app/users/edit-user.php
+++ resources/views/app/users/edit-user.php
@@ -1,89 +1,162 @@
     <?php
-    $title = 'ویرایش مشتری / فروشنده' . $user['user_name'];
+    $title = 'ویرایش مشتری' . $user['name'];
     include_once('resources/views/layouts/header.php');
     include_once('public/alerts/check-inputs.php');
     include_once('public/alerts/error.php');
     ?>
 
-    <!-- Start content -->
     <div class="content">
-        <div class="content-title">ویرایش مشتری / فروشنده <?= $user['user_name'] ?></div>
-
-        <!-- start page content -->
+        <div class="content-title">ثبت مشتری جدید</div>
         <div class="box-container">
             <div class="insert">
-                <form action="<?= url('edit-user-store/' . $user['id']) ?>" method="POST" enctype="multipart/form-data">
-                    <div class="inputs d-flex">
-                        <div class="one">
-                            <div class="label-form mb5 fs14">نام و تخلص <?= _star ?> </div>
-                            <input type="text" class="checkInput" value="<?= $user['user_name'] ?>" name="user_name" placeholder="نام و تخلص را وارد نمایید" maxlength="40" />
-                        </div>
-                        <div class="one">
-                            <div class="label-form mb5 fs14">نام پدر</div>
-                            <input type="text" name="father_name" value="<?= $user['father_name'] ?>" placeholder="نام پدر را وارد نمایید" maxlength="40" />
-                        </div>
-                    </div>
-                    <div class="inputs d-flex">
-                        <div class="one">
-                            <div class="label-form mb5 fs14">شماره <span class="fs12 color-orange">(تلگرام)</span> <?= _star ?> </div>
-                            <input type="number" class="checkInput" value="<?= $user['phone'] ?>" name="phone" placeholder="شماره را وارد نمایید" />
-                        </div>
-                        <div class="one">
-                            <div class="label-form mb5 fs14">رمزعبور </div>
-                            <input type="password" name="password" placeholder="رمزعبور را وارد نمایید" />
-                        </div>
-                    </div>
+                <form action="<?= url('user-store') ?>" method="POST" enctype="multipart/form-data">
 
                     <div class="inputs d-flex">
                         <div class="one">
-                            <div class="label-form mb5 fs14">آدرس</div>
-                            <textarea name="address" placeholder="آدرس را وارد نمایید"><?= $user['address'] ?></textarea>
+                            <div class="label-form mb5 fs14">نام مشتری <?= _star ?> </div>
+                            <input type="text" class="checkInput" name="name" placeholder="نام و تخلص را وارد نمایید" maxlength="40" />
                         </div>
                         <div class="one">
-                            <div class="label-form mb5 fs14">توضیحات</div>
-                            <textarea name="description" placeholder="توضیحات را وارد نمایید"><?= $user['description'] ?></textarea>
+                            <div class="label-form mb5 fs14">شماره <?= _star ?></div>
+                            <input type="text" class="checkInput validate-number" name="phone" placeholder="شماره را وارد نمایید" maxlength="15" />
                         </div>
                     </div>
 
-                    <!-- <div class="inputs d-flex d-none">
-                        <div class="one ">
-                            <label class="d-flex justify-center">
-                                <input type="checkbox" class="checkbox-select" name="is_customer" checked>
-                                <div class="label-form mb5 fs22" for="name">مشتری</div>
-                            </label>
-                        </div>
-                        <div class="one ">
-                            <label class="d-flex justify-center">
-                                <input type="checkbox" class="checkbox-select" name="is_seller" checked>
-                                <div class="label-form mb5 fs22" for="name">فروشنده</div>
-                            </label>
-                        </div>
-                    </div> -->
+                    <div class="accordion-title color-orange mb5">ثبت اندزه‌ها</div>
+                    <div class="accordion-content">
+                        <div class="child-accordioin">
 
-                    <div class="inputs d-flex">
-                        <div class="one">
-                            <div class="label-form mb5 fs14">انتخاب عکس</div>
-                            <input type="file" id="image" name="user_image" accept="image/jpeg, image/png, image/gif">
+                            <div class="inputs d-flex">
+                                <div class="one">
+                                    <div class="label-form mb5 fs14">قد</div>
+                                    <input type="text" placeholder=" اندازه قد را وارد نمایید ..." name="af_height">
+                                </div>
+                                <div class="one">
+                                    <div class="label-form mb5 fs14">شانه</div>
+                                    <input type="text" placeholder="اندازه شانه را وارد نمایید ..." name="af_sholder">
+                                </div>
+                            </div>
+
+                            <div class="inputs d-flex">
+                                <div class="one">
+                                    <div class="label-form mb5 fs14">آستین</div>
+                                    <input type="text" placeholder="اندازه آستین را وارد نمایید ..." name="af_sleeve">
+                                </div>
+                                <div class="one">
+                                    <div class="label-form mb5 fs14">هخن</div>
+                                    <input type="text" placeholder="اندازه هخن را وارد نمایید ..." name="af_ice">
+                                </div>
+                            </div>
+
+                            <div class="inputs d-flex">
+                                <div class="one">
+                                    <div class="label-form mb5 fs14">بغل</div>
+                                    <input type="text" placeholder="اندازه بغل را وارد نمایید ..." name="af_hug">
+                                </div>
+                                <div class="one">
+                                    <div class="label-form mb5 fs14">دامن</div>
+                                    <input type="text" placeholder="اندازه دامن را وارد نمایید ..." name="af_skirt">
+                                </div>
+                            </div>
+
+                            <div class="inputs d-flex">
+                                <div class="one">
+                                    <div class="label-form mb5 fs14">چتی</div>
+                                    <input type="text" placeholder="اندازه چتی را وارد نمایید ..." name="af_chatty">
+                                </div>
+                                <div class="one">
+                                    <div class="label-form mb5 fs14">شلوار</div>
+                                    <input type="text" placeholder="اندازه شلوار را وارد نمایید ..." name="af_pants">
+                                </div>
+                            </div>
+
+                            <div class="inputs d-flex">
+                                <div class="one">
+                                    <div class="label-form mb5 fs14">پارچه</div>
+                                    <input type="text" placeholder="اندازه پارچه را وارد نمایید ..." name="af_cloth">
+                                </div>
+                                <div class="one">
+                                    <div class="label-form mb5 fs14">بر‌ شلوار</div>
+                                    <input type="text" placeholder="اندازه بر شلوار را وارد نمایید ..." name="af_bar_pants">
+                                </div>
+                            </div>
+
+                            <div class="inputs d-flex">
+                                <div class="one">
+                                    <div class="label-form mb5 fs14">مدل</div>
+                                    <input type="text" placeholder="مدل را وارد نمایید ..." name="af_model">
+                                </div>
+                                <div class="one">
+                                    <div class="label-form mb5 fs14">توضیحات دوخت</div>
+                                    <input type="type" placeholder="توضیحات را وارد نمایید ..." name="af_desc">
+                                </div>
+                            </div>
+
                         </div>
                     </div>
-                    <div id="imagePreview">
-                        <img src="" class="img" alt="">
+
+                    <div class="accordion-title color-orange mb5">ثبت اندازه‌های واسکت</div>
+                    <div class="accordion-content">
+                        <div class="child-accordioin">
+
+                            <div class="inputs d-flex">
+                                <div class="one">
+                                    <div class="label-form mb5 fs14">قد</div>
+                                    <input type="text" placeholder="اندزه قد را وارد نمایید ..." name="va_height">
+                                </div>
+                                <div class="one">
+                                    <div class="label-form mb5 fs14">شانه</div>
+                                    <input type="text" placeholder="اندازه شانه را وارد نمایید ..." name="va_sholder">
+                                </div>
+                            </div>
+                            <div class="inputs d-flex">
+                                <div class="one">
+                                    <div class="label-form mb5 fs14">چتی</div>
+                                    <input type="text" placeholder="اندازه چتی را وارد نمایید ..." name="va_chatty">
+                                </div>
+                                <div class="one">
+                                    <div class="label-form mb5 fs14">توضیحات واسکت</div>
+                                    <input type="text" placeholder="توضیحات را وارد نمایید ..." name="va_desc">
+                                </div>
+                            </div>
+
+                        </div>
                     </div>
 
-                    <div>
-                        <img src="<?= ($user['user_image'] ? asset('public/images/users/' . $user['user_image']) : asset('public/assets/img/empty.png')) ?>" class="img" alt="logo">
-                    </div>
-                    <div class="fs11">تصویر فعلی</div>
+                    <div class="accordion-title color-orange">جزئیات بیشتر</div>
+                    <div class="accordion-content">
+                        <div class="child-accordioin">
 
+                            <div class="inputs d-flex">
+                                <div class="one">
+                                    <div class="label-form mb5 fs14">آدرس</div>
+                                    <textarea name="address" placeholder="آدرس را وارد نمایید"></textarea>
+                                </div>
+                                <div class="one">
+                                    <div class="label-form mb5 fs14">توضیحات</div>
+                                    <textarea name="description" placeholder="توضیحات را وارد نمایید"></textarea>
+                                </div>
+                            </div>
+
+                            <div class="inputs d-flex">
+                                <div class="one">
+                                    <div class="label-form mb5 fs14">انتخاب عکس</div>
+                                    <input type="file" id="image" name="image" accept="image/jpeg, image/png, image/gif">
+                                </div>
+                            </div>
+                            <div id="imagePreview" class="mb100">
+                                <img src="" class="img" alt="">
+                            </div>
+
+                        </div>
+                    </div>
 
                     <input type="hidden" name="csrf_token" value="<?= $_SESSION['csrf_token'] ?>" />
-                    <input type="submit" id="submit" value="<?= _edit_btn ?>" class="btn" />
+                    <input type="submit" id="submit" value="ثبت" class="btn" />
                 </form>
             </div>
-            <?= $this->back_link('users') ?>
         </div>
         <!-- end page content -->
     </div>
-    <!-- End content -->
 
     <?php include_once('resources/views/layouts/footer.php') ?>
