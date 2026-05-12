b6b5952362ed540594f8d0fc488caa55fffb6dc5
omidshayan <omidshayan.dev@gmail.com>
2026-05-12 23:26:29 +0430

    add type and model in factor

--- resources/views/app/prints/fish/fish-print.php
+++ resources/views/app/prints/fish/fish-print.php
@@ -1,56 +1,76 @@
-<!DOCTYPE html>
-<html lang="fa">
+<?php
+if (!empty($orderList)) { ?>
+    <html lang="fa">
 
-<head>
-    <meta charset="UTF-8">
-    <title>فاکتور خیاطی</title>
+    <head>
+        <meta charset="UTF-8">
+        <title>فاکتور خیاطی</title>
 
-    <link rel="stylesheet" href="<?= asset('resources/views/app/prints/print.css') ?>" />
-    <link rel="stylesheet" href="<?= asset('resources/views/app/prints/fish.css') ?>" />
+        <link rel="stylesheet" href="<?= asset('resources/views/app/prints/print.css') ?>" />
+        <link rel="stylesheet" href="<?= asset('resources/views/app/prints/fish.css') ?>" />
 
-</head>
+    </head>
+    <?php
+    foreach ($orderList as $order) {
 
-<body>
-    <div class="fish-print color-print">
-        <div class="center fs14">خیاطی و پارچه سرای آرمان</div>
+        $types = [
+            'afghan' => 'لباس افغانی',
+            'vest'   => 'واسکت',
+            'suit'   => 'کت و شلوار',
+        ];
+    ?>
 
-        <hr>
+        <body>
+            <div class="fish-print color-print">
+                <div class="center fs14">خیاطی و پارچه سرای آرمان</div>
 
-        <div class="fs12 p5 color-print justify-between">
-            <div> نام: سیبیب سشیبسیب</div>
-            <div class="fa10">تاریخ: 14025/25/50</div>
-        </div>
-        <hr>
+                <hr>
 
-        <div class="fs12 justify-between p5 bbb">
-            <span>قد: 45</span>
-            <span>شانه: 33</span>
-        </div>
+                <div class="fs12 p5 color-print justify-between">
+                    <div> نام: <?= $user['name'] ?></div>
+                    <div class="fa10">تاریخ: <?= jdate('Y/m/d', strtotime($order['created_at'])) ?></div>
+                </div>
+                <hr>
 
-        <div class="fs12 justify-between p5 bbb">
-            <span>آستین: 45</span>
-            <span>هخن: شانه</span>
-        </div>
-        <div class="fs12 justify-between p5 bbb">
-            <span>بغل: 45</span>
-            <span>دامن: شانه</span>
-        </div>
-        <div class="fs12 justify-between p5 bbb">
-            <span>چتی: 45</span>
-            <span>شلوار: شانه</span>
-        </div>
-        <div class="fs12 justify-between p5 bbb">
-            <span>پارچه: 45</span>
-            <span>بر شلوار: شانه</span>
-        </div>
-        <div class="fs12 p5 bbb">
-            <span>مدل: 45</span>
-        </div>
+                <div class="fs12 p5 bbb">
+                    <span>نوع سفارش: <?= $types[$item['type']] ?></span>
+                </div>
 
-        <div class="fs12 p5 center">تاریخ تحویل: <span>1405/25/10</span></div>
+                <div class="fs12 justify-between p5 bbb">
+                    <span>قد: 45</span>
+                    <span>شانه: 33</span>
+                </div>
 
-    </div>
+                <div class="fs12 justify-between p5 bbb">
+                    <span>آستین: 45</span>
+                    <span>هخن: شانه</span>
+                </div>
+                <div class="fs12 justify-between p5 bbb">
+                    <span>بغل: 45</span>
+                    <span>دامن: شانه</span>
+                </div>
+                <div class="fs12 justify-between p5 bbb">
+                    <span>چتی: 45</span>
+                    <span>شلوار: شانه</span>
+                </div>
+                <div class="fs12 justify-between p5 bbb">
+                    <span>پارچه: 45</span>
+                    <span>بر شلوار: شانه</span>
+                </div>
+                <div class="fs12 p5 bbb">
+                    <span>مدل: <?= $item['model_name'] ?></span>
+                </div>
 
-</body>
+                <div class="fs12 p5 center">تاریخ تحویل: <span>1405/25/10</span></div>
 
-</html>
+            </div>
+
+        </body>
+
+    <?php }
+    ?>
+
+
+    </html>
+<?php }
+?>
