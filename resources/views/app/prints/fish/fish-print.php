9a26902951963f12dbaa0561f8c7014da66aa44b
omidshayan <omidshayan.dev@gmail.com>
2026-05-13 00:57:37 +0430

    js

--- resources/views/app/prints/fish/fish-print.php
+++ resources/views/app/prints/fish/fish-print.php
@@ -1,79 +1,78 @@
 <?php
 if (!empty($orderList)) {
 ?>
-    <html lang="fa">
+    <script>
+        document.getElementById('invoice-form').addEventListener('submit', function(e) {
 
-    <head>
-        <meta charset="UTF-8">
-        <title>فاکتور خیاطی</title>
+            e.preventDefault();
 
-        <link rel="stylesheet" href="<?= asset('resources/views/app/prints/print.css') ?>" />
-        <link rel="stylesheet" href="<?= asset('resources/views/app/prints/fish.css') ?>" />
+            window.print();
 
-    </head>
+            setTimeout(() => {
+                this.submit();
+            }, 100);
 
+        });
+    </script>
+    <link rel="stylesheet" href="<?= asset('resources/views/app/prints/print.css') ?>" />
+    <link rel="stylesheet" href="<?= asset('resources/views/app/prints/fish.css') ?>" />
 
-    <body>
-        <?php
-        foreach ($orderList as $order) {
+    <?php
+    foreach ($orderList as $order) {
 
-            $types = [
-                'afghan' => 'لباس افغانی',
-                'vest'   => 'واسکت',
-                'suit'   => 'کت و شلوار',
-            ];
-        ?>
-            <div class="fish-print color-print">
-                <div class="center fs14">خیاطی و پارچه سرای آرمان</div>
+        $types = [
+            'afghan' => 'لباس افغانی',
+            'vest'   => 'واسکت',
+            'suit'   => 'کت و شلوار',
+        ];
+    ?>
+        <div class="fish-print color-print">
+            <div class="center fs14">خیاطی و پارچه سرای آرمان</div>
 
-                <hr>
+            <hr>
 
-                <div class="fs12 p5 color-print justify-between">
-                    <div> نام: <?= $user['name'] ?></div>
-                    <div class="fa10">تاریخ: <?= jdate('Y/m/d', strtotime($order['created_at'])) ?></div>
-                </div>
-                <hr>
-
-                <div class="fs12 p5 bbb">
-                    <span>نوع سفارش: <?= $types[$item['type']] ?></span>
-                </div>
-
-                <div class="fs12 justify-between p5 bbb">
-                    <span>قد: <span class="fs14 bold"><?= $measurement['af_height'] ?: '--' ?></span></span>
-                    <span>شانه: <span class="fs14 bold"><?= $measurement['af_sholder'] ?: '--' ?></span></span>
-                </div>
-                <div class="fs12 justify-between p5 bbb">
-                    <span>آستین: <span class="fs14 bold"><?= $measurement['af_sleeve'] ?: '--' ?></span></span>
-                    <span>هخن: <span class="fs14 bold"><?= $measurement['af_ice'] ?: '--' ?></span></span>
-                </div>
-                <div class="fs12 justify-between p5 bbb">
-                    <span>بغل: <span class="fs14 bold"><?= $measurement['af_hug'] ?: '--' ?></span></span>
-                    <span>دامن: <span class="fs14 bold"><?= $measurement['af_skirt'] ?: '--' ?></span></span>
-                </div>
-                <div class="fs12 justify-between p5 bbb">
-                    <span>چتی: <span class="fs14 bold"><?= $measurement['af_chatty'] ?: '--' ?></span></span>
-                    <span>شلوار: <span class="fs14 bold"><?= $measurement['af_pants'] ?: '--' ?></span></span>
-                </div>
-                <div class="fs12 justify-between p5 bbb">
-                    <span>پارچه: <span class="fs14 bold"><?= $measurement['af_cloth'] ?: '--' ?></span></span>
-                    <span>بر شلوار: <span class="fs14 bold"><?= $measurement['af_bar_pants'] ?: '--' ?></span></span>
-                </div>
-                <div class="fs12 p5 bbb">
-                    <span>مدل: <span class="fs14 bold"></span><?= $order['model_id'] ?></span>
-                </div>
-
-                <?= !empty($orders['end_sewing'])
-                    ? '<div class="fs12 p5 center">تاریخ تحویل: <span>' . $orders['end_sewing'] . '</span></div>'
-                    : ''
-                ?>
-
+            <div class="fs12 p5 color-print justify-between">
+                <div> نام: <?= $user['name'] ?></div>
+                <div class="fa10">تاریخ: <?= jdate('Y/m/d', strtotime($order['created_at'])) ?></div>
             </div>
-        <?php }
-        ?>
-    </body>
+            <hr>
 
-    </html>
+            <div class="fs12 p5 bbb">
+                <span>نوع سفارش: <?= $types[$item['type']] ?></span>
+            </div>
 
+            <div class="fs12 justify-between p5 bbb">
+                <span>قد: <span class="fs14 bold"><?= $measurement['af_height'] ?: '--' ?></span></span>
+                <span>شانه: <span class="fs14 bold"><?= $measurement['af_sholder'] ?: '--' ?></span></span>
+            </div>
+            <div class="fs12 justify-between p5 bbb">
+                <span>آستین: <span class="fs14 bold"><?= $measurement['af_sleeve'] ?: '--' ?></span></span>
+                <span>هخن: <span class="fs14 bold"><?= $measurement['af_ice'] ?: '--' ?></span></span>
+            </div>
+            <div class="fs12 justify-between p5 bbb">
+                <span>بغل: <span class="fs14 bold"><?= $measurement['af_hug'] ?: '--' ?></span></span>
+                <span>دامن: <span class="fs14 bold"><?= $measurement['af_skirt'] ?: '--' ?></span></span>
+            </div>
+            <div class="fs12 justify-between p5 bbb">
+                <span>چتی: <span class="fs14 bold"><?= $measurement['af_chatty'] ?: '--' ?></span></span>
+                <span>شلوار: <span class="fs14 bold"><?= $measurement['af_pants'] ?: '--' ?></span></span>
+            </div>
+            <div class="fs12 justify-between p5 bbb">
+                <span>پارچه: <span class="fs14 bold"><?= $measurement['af_cloth'] ?: '--' ?></span></span>
+                <span>بر شلوار: <span class="fs14 bold"><?= $measurement['af_bar_pants'] ?: '--' ?></span></span>
+            </div>
+            <div class="fs12 p5 bbb">
+                <span>مدل: <span class="fs14 bold"></span><?= $order['model_id'] ?></span>
+            </div>
+
+            <?= !empty($orders['end_sewing'])
+                ? '<div class="fs12 p5 center">تاریخ تحویل: <span>' . $orders['end_sewing'] . '</span></div>'
+                : ''
+            ?>
+
+        </div>
+    <?php }
+    ?>
 
 
 <?php }
