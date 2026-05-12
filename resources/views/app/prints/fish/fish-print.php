ef0204e3a05385517469c54496c13e696e659edb
omidshayan <omidshayan.dev@gmail.com>
2026-05-12 23:14:22 +0430

    end v1 factor

--- resources/views/app/prints/fish/fish-print.php
+++ resources/views/app/prints/fish/fish-print.php
@@ -11,18 +11,43 @@
 </head>
 
 <body>
-    <div class="fish-print">
-        <div class="center fs14 color-print">خیاطی و پارچه سرای آرمان</div>
+    <div class="fish-print color-print">
+        <div class="center fs14">خیاطی و پارچه سرای آرمان</div>
 
-        <hr class="tt">
+        <hr>
 
         <div class="fs12 p5 color-print justify-between">
-           <div> نام: سیبیب سشیبسیب</div>
-           <div class="fa10">تاریخ: 14025/25/50</div>
+            <div> نام: سیبیب سشیبسیب</div>
+            <div class="fa10">تاریخ: 14025/25/50</div>
+        </div>
+        <hr>
+
+        <div class="fs12 justify-between p5 bbb">
+            <span>قد: 45</span>
+            <span>شانه: 33</span>
         </div>
 
-        
+        <div class="fs12 justify-between p5 bbb">
+            <span>آستین: 45</span>
+            <span>هخن: شانه</span>
+        </div>
+        <div class="fs12 justify-between p5 bbb">
+            <span>بغل: 45</span>
+            <span>دامن: شانه</span>
+        </div>
+        <div class="fs12 justify-between p5 bbb">
+            <span>چتی: 45</span>
+            <span>شلوار: شانه</span>
+        </div>
+        <div class="fs12 justify-between p5 bbb">
+            <span>پارچه: 45</span>
+            <span>بر شلوار: شانه</span>
+        </div>
+        <div class="fs12 p5 bbb">
+            <span>مدل: 45</span>
+        </div>
 
+        <div class="fs12 p5 center">تاریخ تحویل: <span>1405/25/10</span></div>
 
     </div>
 
